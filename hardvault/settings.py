bf5ac410eea29684fc189d4659f45d3770c0e3b4
Aayush Prasad <aayushp336@gmail.com>
2025-12-09 01:17:09 +0530

    Bug fix

--- hardvault/settings.py
+++ hardvault/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'client',
     'owner',
 ]
@@ -136,3 +137,20 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom User Model
+AUTH_USER_MODEL = 'accounts.User'
+
+# Email Configuration (Gmail SMTP)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dev.aayushdroid@gmail.com'  
+EMAIL_HOST_PASSWORD = 'kvfgmhynkoblhqbh' 
+DEFAULT_FROM_EMAIL = 'dev.aayushdroid@gmail.com'
+
+# Login URLs
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/accounts/client/dashboard/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
